b8f3d99a7f4d6f358ba3b22ff6480680fc43ee5c
Olle Thalén <olle.thalen@omegapoint.se>
2016-10-07 15:38:03 +0200

    build update

--- tasks/build.js
+++ tasks/build.js
@@ -1,6 +1,7 @@
 var Builder = require('jspm').Builder;
 var builder = new Builder();
 
+console.log("Starting application build...");
 builder.buildStatic("app", "build/build.js", { rollup: true })
     .then(function() {
         console.log('Build complete');
